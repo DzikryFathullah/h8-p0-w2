3da442ef2fa180b9322b15ae19625cb43172185f
DzikryFathullah <40556489+DzikryFathullah@users.noreply.github.com>
2018-07-06 20:47:09 +0700

    Update exercise3.js
    Revisi : Tidak menggunakan switch case tapi if-else untuk problem solvingnya

--- exercise3.js
+++ exercise3.js
@@ -1,13 +1,17 @@
-var nama = 'Dzikry';
-var peran = 'penyihir';
-if (nama=='' || peran=='') {
+var nama = 'dzikry';
+var peran = 'tabib';
+if (nama==='' || peran==='') {
   console.log('Nama harus diisi!!');
-}else switch (peran){
-  case 'ksatria': {console.log('Selamat datang di dunia Proxytia, ' + nama);
-  console.log('Halo ' + peran + ' ' + nama + ', kamu dapat menyerang dengan senjatamu');break;}
-  case 'tabib': {console.log('Selamat datang di dunia Proxytia, ' + nama);
-  console.log('Halo ' + peran + ' ' + nama + ', kamu akan membantu temanmu yang terluka');break;}
-  case 'penyihir': {console.log('Selamat datang di dunia Proxytia, ' + nama);
-  console.log('Halo ' + peran + ' ' + nama + ', ciptakan keajaiban yang membantu kemenanganmu!');break;}
-  default: {console.log('Hallo '+ nama + ', pilih peranmu untuk memulai game!');}
-}
+}else if (peran === 'ksatria'){
+   console.log('Selamat datang di dunia Proxytia, ' + nama);
+   console.log('Halo ' + peran + ' ' + nama + ', kamu dapat menyerang dengan senjatamu');
+ } else if (peran === 'tabib'){
+   console.log('Selamat datang di dunia Proxytia, ' + nama);
+   console.log('Halo ' + peran + ' ' + nama + ', kamu akan membantu temanmu yang terluka');
+ } else if (peran === 'penyihir'){
+   console.log('Selamat datang di dunia Proxytia, ' + nama);
+   console.log('Halo ' + peran + ' ' + nama + ', ciptakan keajaiban yang membantu kemenanganmu!');
+ } else {
+  console.log('Hallo '+ nama + ', pilih peranmu untuk memulai game!');
+ } le.log('Hallo '+ nama + ', pilih peranmu untuk memulai game!');
+ } 
